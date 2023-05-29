bc569f12f9211d819ab46f8e0b849a2d3bec777e
Kl4y4 <surowiec.bartosz@wp.pl>
2023-05-29 03:57:32 +0200

    Converted Form into a controlled component

--- src/app.tsx
+++ src/app.tsx
@@ -9,16 +9,29 @@ function App() {
 
 const Form = () => {
 
+  const [dishProperties, setProperties] = useState(['', '', '', '']);
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(dishProperties);
+  }
+
+  const onChange = (e, i) => {
+    const updatedDish = dishProperties.map((el, ind) => ind == i ? e.target.value : el);
+    setProperties(updatedDish);
+  }
 
   return <>
-    <form>
-      <input type="text" />
-      <input type="time" list="preparation-time-suggestions" step="1" min="00:00:01" max="99:99:99" />
-      <select>
-        <option value="1">pizza</option>
-        <option value="2">soup</option>
-        <option value="3">sandwich</option>
+    <form onSubmit={onSubmit}>
+      <input type="text" value={dishProperties[0]} onChange={(e) => {onChange(e, 0)}} />
+      <input type="time" list="preparation-time-suggestions" 
+      step="1" min="00:00:01"
+      value={dishProperties[1]} onChange={(e) => {onChange(e, 1)}} />
+      <select
+      value={dishProperties[2]} onChange={(e) => {onChange(e, 2)}}>
+        <option value="pizza">pizza</option>
+        <option value="soup">soup</option>
+        <option value="sandwich">sandwich</option>
       </select>
       <input type="submit" value="Send" />
     </form>
