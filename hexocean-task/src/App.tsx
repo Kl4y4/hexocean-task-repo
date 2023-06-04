49ec71083f2ea12a2a74fd4f8cc01c80c9ccb93c
Kl4y4 <surowiec.bartosz@wp.pl>
2023-06-04 21:42:24 +0200

    Project initialized with CRA

---
+++ hexocean-task/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        <p>
+          Hello world!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
