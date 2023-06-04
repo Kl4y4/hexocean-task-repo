e90b17562233f29a9ba24f1644f1e2b7be01879c
Kl4y4 <surowiec.bartosz@wp.pl>
2023-06-04 21:30:32 +0200

    Dele

--- src/types/dishTypes.d.ts
+++
@@ -1,9 +0,0 @@
-type Dish = {
-    name: string,
-    preparation_time: string,
-    type: string,
-    no_of_slices?: number,
-    diameter?: number,
-    spiciness_scale?: number,
-    slices_of_bread?: number
-}
