8bfbc010a38cc2bd80842aac2b1631fb3ef00c82
Kl4y4 <surowiec.bartosz@wp.pl>
2023-06-04 18:48:52 +0200

    Changed the state logic to use useReducer ( ;) )

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,37 @@
 const React = window.React;
-const { useState } = window.React;
+const { useState, useReducer } = window.React;
 const { Dish, Pizza, Sandwich, Soup } = window.ts;
+
+function reducer(state, action) {
+  switch (action.property) {
+    case 'name':
+      return { ...state, name: action.payload.value}
+    case 'preparation_time':
+      return { ...state, preparation_time: action.payload.value}
+    case 'type':
+      return { ...state, type: action.payload.value}
+    case 'no_of_slices':
+      delete state.spiciness_scale;
+      delete state.slices_of_bread;
+      return { ...state, no_of_slices: action.payload.value}
+    case 'diameter':
+      delete state.spiciness_scale;
+      delete state.slices_of_bread;
+      return { ...state, diameter: action.payload.value}
+    case 'spiciness_scale':
+      delete state.no_of_slices;
+      delete state.diameter;
+      delete state.slices_of_bread;
+      return { ...state, spiciness_scale: action.payload.value}
+    case 'slices_of_bread':
+      delete state.no_of_slices;
+      delete state.diameter;
+      delete state.spiciness_scale;
+      return { ...state, slices_of_bread: action.payload.value}
+    default:
+      return state;
+  }
+}
 
 function App() {
   return <>
@@ -11,66 +42,74 @@ function App() {
 
 const Form = () => {
 
-  const [name, setName] = useState('');
-  const [preparation_time, setPreparationTime] = useState('');
-  const [type, setType] = useState('');
-  const [no_of_slices, setNoOfSlices] = useState(1);
-  const [diameter, setDiameter] = useState(1);
-  const [spiciness_scale, setSpicinessScale] = useState(1);
-  const [slices_of_bread, setSlicesOfBread] = useState(1);
-  const setters = {
-    setName,
-    setPreparationTime,
-    setType,
-    setNoOfSlices,
-    setDiameter,
-    setSpicinessScale,
-    setSlicesOfBread
-  }
+  const [state, dispatch] = useReducer(reducer, {});
 
-  const currentDish : typeof Dish = {
-    name,
-    preparation_time,
-    type,
-    no_of_slices,
-    diameter,
-    spiciness_scale,
-    slices_of_bread
-  }
+  // const [name, setName] = useState('');
+  // const [preparation_time, setPreparationTime] = useState('');
+  // const [type, setType] = useState('');
+  // const [no_of_slices, setNoOfSlices] = useState(1);
+  // const [diameter, setDiameter] = useState(1);
+  // const [spiciness_scale, setSpicinessScale] = useState(1);
+  // const [slices_of_bread, setSlicesOfBread] = useState(1);
+  // const setters = {
+  //   setName,
+  //   setPreparationTime,
+  //   setType,
+  //   setNoOfSlices,
+  //   setDiameter,
+  //   setSpicinessScale,
+  //   setSlicesOfBread
+  // }
+
+  // const currentDish : typeof Dish = {
+  //   name,
+  //   preparation_time,
+  //   type,
+  //   no_of_slices,
+  //   diameter,
+  //   spiciness_scale,
+  //   slices_of_bread
+  // }
 
   const onSubmit = (e : React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    console.log(currentDish);
+    console.log(state);
   }
 
+  // const onChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+  //   setters["set" + e.target.name](e.target.value);
+  //   console.log();
+  // }
+
   const onChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setters["set" + e.target.name](e.target.value);
-    console.log();
+    let propertyName = e.target.name;
+    let newValue = e.target.value;
+    dispatch({ property: propertyName, payload: { value: newValue }})
   }
 
   return <>
     <form onSubmit={onSubmit} id="form">
-      <input name="Name" type="text"
-      value={name} onChange={onChange} />
-      <input name="PreparationTime" type="time" list="preparation-time-suggestions" 
+      <input name="name" type="text"
+      value={state.name || ""} onChange={onChange} />
+      <input name="preparation_time" type="time" list="preparation-time-suggestions" 
       step="1" min="00:00:01" 
-      value={preparation_time} onChange={onChange} />
-      <select name="Type"
-      value={type} onChange={onChange}>
+      value={state.preparation_time || ""} onChange={onChange} />
+      <select name="type"
+      value={state.type || ""} onChange={onChange}>
         <option value="pizza">pizza</option>
         <option value="soup">soup</option>
         <option value="sandwich">sandwich</option>
       </select>
 
-      {type == "pizza" && 
+      {state.type == "pizza" && 
         <>
-          <input name="NoOfSlices" type="number" value={no_of_slices} onChange={onChange} required />
-          <input name="Diameter" type="number" value={diameter} onChange={onChange} required />
+          <input name="no_of_slices" type="number" value={state.no_of_slices || ""} onChange={onChange} required />
+          <input name="diameter" type="number" value={state.diameter || ""} onChange={onChange} required />
         </>
       }
-      {type == "soup" && <input name="SpicinessScale" type="number" value={spiciness_scale} onChange={onChange} required />}
-      {type == "sandwich" && <input name="SlicesOfBread" type="number" value={slices_of_bread} onChange={onChange} required />}
-      
+      {state.type == "soup" && <input name="spiciness_scale" type="number" value={state.spiciness_scale || ""} onChange={onChange} required />}
+      {state.type == "sandwich" && <input name="slices_of_bread" type="number" value={state.slices_of_bread || ""} onChange={onChange} required />}
+
       <input type="submit" value="Send" />
     </form>
   </>
