d6432dd00ffe3c08d3d821eba5c1d287f14d52f0
Kl4y4 <surowiec.bartosz@wp.pl>
2023-06-04 17:44:56 +0200

    Added custom type

---
+++ src/types/dishTypes.d.ts
@@ -0,0 +1,9 @@
+type Dish = {
+    name: string,
+    preparation_time: string,
+    type: string,
+    no_of_slices?: number,
+    diameter?: number,
+    spiciness_scale?: number,
+    slices_of_bread?: number
+}
